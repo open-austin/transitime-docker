5220e2e50c0e4dc05fcc0ec3d22b3d214e6cecc0
Nathan Walker <nathan@rylath.net>
2015-10-01 21:54:48 -0500

    Restructured scripts

--- generate.sh
+++
@@ -1,5 +0,0 @@
-# Interestingly, the shell docker is running in doesn't like empty lines
-createdb -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres cap-metro
-psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -d cap-metro -f /transitime-sql/ddl_postgres_org_transitime_db_structs.sql
-psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -d cap-metro -f /transitime-sql/ddl_postgres_org_transitime_db_webstructs.sql
-# TODO: import cap metro
--- go.sh
+++ go.sh
@@ -1,4 +1,5 @@
 export PGPASSWORD=transitime
+docker build -t transitime-server .
 docker run --name transitime-db -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres
 docker run --name transitime --link transitime-db:postgres -p 8080:8080 -d transitime-server
 
