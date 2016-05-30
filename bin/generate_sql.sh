6a260432cac64be66c88c9aacdd312dfbf729fb5
Sean Óg Crudden <og.crudden@gmail.com>
2016-05-30 13:35:57 +0100

    Update generate_sql.sh
    create webstructs tables too. It was a typo.

--- bin/generate_sql.sh
+++ bin/generate_sql.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 java -jar $TRANSITIMECORE/transitime/target/SchemaGenerator.jar -p org.transitime.db.structs -o /usr/local/transitime/db/
-java -jar $TRANSITIMECORE/transitime/target/SchemaGenerator.jar -p org.transitime.db.structs -o /usr/local/transitime/db/
+java -jar $TRANSITIMECORE/transitime/target/SchemaGenerator.jar -p org.transitime.db.webstructs -o /usr/local/transitime/db/
 
 	
 
